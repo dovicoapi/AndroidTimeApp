a6cb3b764f6b1d7f60793831d3db3a66e6002b12
ggallant <ggallant@dovico.com>
2012-05-31 12:36:23 -0300

    Made some changes to how time entries are pulled for the Timesheet view (it was pulling all time entries from the beginning of time for a user rather than specifying a date range)

--- DOVICO/src/com/dovico/timesheet/common/DOVICOConst.java
+++ DOVICO/src/com/dovico/timesheet/common/DOVICOConst.java
@@ -4,4 +4,6 @@ public class DOVICOConst {
 	
 	public static String DEVELOPERS_CONSUMER_SECRET_API_TOKEN = "{Your Consumer Secret Here}";
 
+	// The REST API returns and expects dates in this format
+	public static String XML_DATE_FORMAT = "yyyy-MM-dd";
 }
--- DOVICO/src/com/dovico/timesheet/rest/methods/GetTimeEntriesForEmployee.java
+++ DOVICO/src/com/dovico/timesheet/rest/methods/GetTimeEntriesForEmployee.java
@@ -2,6 +2,7 @@ package com.dovico.timesheet.rest.methods;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
@@ -13,6 +14,7 @@ import android.content.Context;
 import android.util.Log;
 
 import com.dovico.timesheet.beans.TimeEntry;
+import com.dovico.timesheet.common.DOVICOConst;
 import com.dovico.timesheet.common.DOVICORestAPI;
 import com.dovico.timesheet.common.TimeEntryComparatorByDate;
 import com.dovico.timesheet.rest.RestClient;
@@ -37,12 +39,39 @@ public class GetTimeEntriesForEmployee extends RestMethod {
 	 * @param context
 	 *            of application
 	 */
+	/// <history>
+	/// <modified author="C. Gerard Gallant" date="2012-05-31" reason="Fixed a bug where the URI was requesting ALL time entries for the person rather than just the time entries within the expected date range (depending on how long the person has been tracking their time with DOVICO software, the list of time entries could be huge)"/>
+	/// </history>
 	public GetTimeEntriesForEmployee(Context context, String employeeID, String userToken, String restAPIVersion) {
 		super(context, userToken, restAPIVersion);
-		methodUrl = DOVICORestAPI.RestURL.GET_TIME_ENTRIES + employeeID + DOVICORestAPI.RestURL.DOVICO_API_VERSION + restAPIVersion;
+
+		// The end date for the time entries we want is today. The Start date is 6 days before today
+		Date dtEndDate = new Date();
+		Date dtStartDate = new Date();
+		dtStartDate.setTime(dtEndDate.getTime() - (1000 * 60 * 60 *24 * 6)); // 1000 milliseconds per second x 60 seconds per minute x 60 minutes per hour x 24 hours per day x 6 days  
+		
+		// Build the Date range query string
+		String sDateRangeQueryString = buildDateRangeQueryString(dtStartDate, dtEndDate); 
+
+		// Build up the URI needed to request the user's time entries
+		methodUrl = (DOVICORestAPI.RestURL.GET_TIME_ENTRIES + employeeID + DOVICORestAPI.RestURL.DOVICO_API_VERSION + restAPIVersion + "&" + sDateRangeQueryString);
 		restClient = new RestClient(methodUrl);
 	}
+	
+	
+	/// <history>
+	/// <modified author="C. Gerard Gallant" date="2012-05-31" reason="Copied from the common library"/>
+	/// </history>
+	// Helper to return a Date Range query string
+	private String buildDateRangeQueryString(Date dtDateRangeStart, Date dtDateRangeEnd){
+		// Create a Date formatter object that will turn a date into the XML Date Format string expected by the API
+		SimpleDateFormat fFormatter = new SimpleDateFormat(DOVICOConst.XML_DATE_FORMAT);
+		
+		// Return the date range query string with an encoded space between both dates
+		return ("daterange=" + fFormatter.format(dtDateRangeStart) + "%20" + fFormatter.format(dtDateRangeEnd));
+	}
 
+	
 	@Override
 	protected void doInBackground() {
 		try {
